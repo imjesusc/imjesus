8cf32e0f88c64a429687473b0b019cf38bee95a1
imjesusc <imjesusc.ds@gmail.com>
2024-04-27 13:55:47 -0400

    🎉: Init porfolio setup

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import RootPage from './pages/root'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <RootPage />
+  </React.StrictMode>
+)
---
+++ tailwind.config.js
@@ -0,0 +1,81 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  darkMode: ['class'],
+  content: [
+    'index.html',
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}'
+  ],
+  prefix: '',
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        tabletlg: '1024px'
+      }
+    },
+    extend: {
+      boxShadow: {
+        medium: '0px 0px 15px 0px rgba(0,0,0,.03),0px 2px 30px 0px rgba(0,0,0,.08),0px 0px 1px 0px rgba(0,0,0,.3)'
+      },
+      fontFamily: {
+        geist: ['Geist', 'sans-serif']
+      },
+      colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))'
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))'
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))'
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))'
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))'
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))'
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))'
+        }
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      }
+    },
+    screens: {
+      phone: '414px',
+      mobile: '640px',
+      tablet: '768px',
+      tabletlg: '960px',
+      tabletxl: '1024px',
+      laptop: '1200px',
+      laptoplg: '1400px',
+      desktop: '1700px'
+    }
+  }
+}
+
+export default config
