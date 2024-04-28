4c8c49402360f4ea26d9a7a9567efe653dca8ab5
imjesusc <imjesusc.ds@gmail.com>
2024-04-28 15:07:33 -0400

    refactor: Update ExternalLink UI component to utilize forwardRef

--- src/components/ui/external-link.jsx
+++ src/components/ui/external-link.jsx
@@ -1,19 +1,27 @@
 import { cn } from '@/utilities/cn'
+import { ArrowTopRightIcon } from '@radix-ui/react-icons'
+import { forwardRef } from 'react'
 
-export const ExternalLink = ({ href, className, children, ...props }) => {
+const ExternalLink = forwardRef(({ href, className, showArrow, children, ...props }, ref) => {
   return (
     <a
       href={href}
       target="_blank"
       rel="noreferrer"
       className={cn(
-        'gap-2 text-sm underline decoration-muted-foreground/50 decoration-1',
+        'flex cursor-pointer items-center gap-1 underline decoration-muted-foreground/50 decoration-1',
         'underline-offset-[3px] transition-colors hover:decoration-muted-foreground',
         className
       )}
+      ref={ref}
       {...props}
     >
       {children}
+      {showArrow && <ArrowTopRightIcon className="h-4 w-4 text-muted-foreground/50" />}
     </a>
   )
-}
+})
+
+ExternalLink.displayName = 'ExternalLink'
+
+export default ExternalLink
